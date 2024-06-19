24657bbe461d627b37deebe0d15236bf8ade255d
ervandew <ervandew@gmail.com>
2024-06-19 10:03:09 -0600

    fix possible infinite loop + linter changes

--- autoload/regex/lang/regex.lua
+++ autoload/regex/lang/regex.lua
@@ -1,19 +1,21 @@
 local file = io.open(arg[1])
-local pattern = file:read()
-if pattern ~= nil then
-  local pos = #pattern
-  for line in file:lines() do
-    local index = 1
-    while true do
-      s, e = string.find(line, pattern, index)
-      if s == nill then
-        break
+if file ~= nil then
+  local pattern = file:read()
+  if pattern ~= nil then
+    local pos = #pattern
+    for line in file:lines() do
+      local index = 1
+      while true do
+        local s, e = string.find(line, pattern, index)
+        if s == nil then
+          break
+        end
+        print((s + pos) .. '-' .. (e + pos))
+        index = e + 1
       end
-      print((s + pos) .. '-' .. (e + pos))
-      index = e
+      pos = pos + #line + 1
     end
-    pos = pos + #line + 1
   end
-end
 
-file:close()
+  file:close()
+end
